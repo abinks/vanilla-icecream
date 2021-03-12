d1eac39297ca54219c259d60ec61b30fb812df02
abinks <allison.binkley@asurion.com>
2021-03-12 11:22:39 -0600

    build

--- src/app.js
+++ src/app.js
@@ -1,16 +1,15 @@
 import { hot } from 'react-hot-loader/root';
-import React, { useState } from 'react';
+import React from 'react';
 
 
 function App(props) {
-    const [counter, setCounter] = useState(0)
 
     return (
         <>
             <div>
-                {counter}
+                welcome to abinks.github.io
             </div>
-            <button onClick={() => setCounter(counter + 1)}>Testing</button>
+            
         </>
     );
 }
